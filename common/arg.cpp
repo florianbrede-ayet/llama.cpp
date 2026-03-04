870238322fc33d8b70138dc90d3f35d8f4db0190
Piotr Wilkin <piotr.wilkin@syndatis.com>
2026-03-04 14:53:03 +0100

    Change to `--checkpoint-every-n-tokens` (short form `-cpent`), add short form `-ctxcp` for `--ctx-checkpoints`)

--- common/arg.cpp
+++ common/arg.cpp
@@ -1279,7 +1279,7 @@ common_params_context common_params_parser_init(common_params & params, llama_ex
         }
     ).set_env("LLAMA_ARG_SWA_FULL"));
     add_opt(common_arg(
-        {"--ctx-checkpoints", "--swa-checkpoints"}, "N",
+        {"-ctxcp", "--ctx-checkpoints", "--swa-checkpoints"}, "N",
         string_format("max number of context checkpoints to create per slot (default: %d)"
             "[(more info)](https://github.com/ggml-org/llama.cpp/pull/15293)", params.n_ctx_checkpoints),
         [](common_params & params, int value) {
@@ -1287,12 +1287,12 @@ common_params_context common_params_parser_init(common_params & params, llama_ex
         }
     ).set_env("LLAMA_ARG_CTX_CHECKPOINTS").set_examples({LLAMA_EXAMPLE_SERVER, LLAMA_EXAMPLE_CLI}));
     add_opt(common_arg(
-        {"--checkpoint-every-nb"}, "N",
-        string_format("create a checkpoint every n batches during prefill (processing), -1 to disable (default: %d)", params.checkpoint_every_nb),
+        {"-cpent", "--checkpoint-every-n-tokens"}, "N",
+        string_format("create a checkpoint every n tokens during prefill (processing), -1 to disable (default: %d)", params.checkpoint_every_nt),
         [](common_params & params, int value) {
-            params.checkpoint_every_nb = value;
+            params.checkpoint_every_nt = value;
         }
-    ).set_env("LLAMA_ARG_CHECKPOINT_EVERY_NB").set_examples({LLAMA_EXAMPLE_SERVER, LLAMA_EXAMPLE_CLI}));
+    ).set_env("LLAMA_ARG_CHECKPOINT_EVERY_NT").set_examples({LLAMA_EXAMPLE_SERVER, LLAMA_EXAMPLE_CLI}));
     add_opt(common_arg(
         {"-cram", "--cache-ram"}, "N",
         string_format("set the maximum cache size in MiB (default: %d, -1 - no limit, 0 - disable)"
